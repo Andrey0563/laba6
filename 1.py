ad42618dfdbdd452ce72d74705820d3245531127
Andrey <duzhak.a55@gmail.com>
2020-02-14 14:49:13 +0200

    1.py

--- 1.py
+++ 1.py
@@ -1,45 +1,74 @@
-from enum import Enum 
- 
-class month(Enum): 
-    January = 1 
-    February = 2 
-    March = 3 
-    April = 4 
-    May = 5 
-    June = 6 
-    July = 7 
-    August = 8 
-    September = 9 
-    October = 10 
-    November = 11 
-    December = 12 
- 
+days = range (1, 32)
+mounths = range (1, 13)
+years = range (1901, 2020)
+while True:
+    while True:
+        try:
+            d = int(input('Введіть день: '))
+            m = int(input('Введіть місяць: '))
+            y = int(input('Ведіть рік: '))
+            break
+        except ValueError:
+            print('Enter correct number')
+    if d in days and m in mounths and y in years:
+      if y%4==0:
+        while m < 1 or m > 12:
+           m= int(input("input m!:"))
+        if m == 2:
+         index = 1
+        elif m == 4 or m == 6 or m == 9 or m == 11:
+         index = 2
+        elif m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
+         index = 3
+    
+        while d < 1 or (d > 29 and index == 1) or (d > 30 and index == 2) or (d > 31 and index == 3):
+         d = int(input("input d!:"))
 
-class season(Enum): 
-    Winter = 1 
-    Spring = 2 
-    Summer = 3 
-    Autumn = 4 
+        if (d == 29 and index == 1) or (d == 30 and index == 2) or (d == 31 and index == 3):
+         dali = 1
+        else:
+         dali =d+1
+        if (d==1 and index==1) or (d==1 and index==2):
+         nazad=31
+        elif (m==3 and d==1):
+         nazad=29
+        elif (m==5 and d==1) or (m==7 and d==1) or (m==10 and d==1) or (m==12 and d==1):
+         nazad=30
+        else:
+         nazad=d-1
+        
+        print(dali, ".", nazad)
  
-while True: 
-    while True: 
-        try: 
-            s = int(input("month: ")) 
-            break 
-        except ValueError: 
-            print('Error') 
-    if s in range(1, 3) or s == 12: 
-        print('Winter') 
-    elif s in range(3, 6): 
-        print("Spring") 
-    elif s in range(6, 8): 
-        print('Summer') 
-    elif s in range(9, 12): 
-        print('Autumn') 
-    else:
-        print('Такого місяця не існує')
+      else:
+         nazad=0
+         while m < 1 or m > 12:
+          m= int(input("input m!:"))
+         if m == 2:
+          index = 1
+         elif m == 4 or m == 6 or m == 9 or m == 11:
+           index = 2
+         elif m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
+           index = 3
+    
+         while d < 1 or (d > 28 and index == 1) or (d > 30 and index == 2) or (d > 31 and index == 3):
+           d = int(input("input d!:"))
+
+         if (d == 28 and index == 1) or (d == 30 and index == 2) or (d == 31 and index == 3):
+          dali = 1
+         else:
+          dali =d+1
+         if (d==1 and index==1) or (d==1 and index==2):
+          nazad=31
+         elif (m==3 and d==1):
+          nazad=28
+         elif (m==5 and d==1) or (m==7 and d==1) or (m==10 and d==1) or (m==12 and d==1):
+          nazad==30
+         else:
+          nazad=d-1
+        
+         print(dali, ".", nazad)
     result = input('Хочете запустити заново? Якщо так - 1, ні - інше: ')
     if result == '1':
-     continue
+        continue
     else:
         break
